2fd3fd36bcf17ae60e7f78a939ad627f30e9ca1a
Sandhya <sandhya@clutterbot.com>
2025-08-05 22:04:29 +0530

    bug fix - always resyume

--- src/lerobot/utils/wandb_utils.py
+++ src/lerobot/utils/wandb_utils.py
@@ -90,7 +90,7 @@ class WandBLogger:
             # save_code=False,
             # TODO(rcadene): split train and eval, and run async eval with job_type="eval"
             # job_type="train_eval",
-            resume="must" #if cfg.resume else None,
+            resume="never" #if cfg.resume else None,
             # mode=self.cfg.mode if self.cfg.mode in ["online", "offline", "disabled"] else "online",
         )
         run_id = wandb.run.id
