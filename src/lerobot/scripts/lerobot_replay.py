303635457a61dfa5459f7f25c45e267ab52b7a84
Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
2025-12-03 12:09:24 +0100

    feat(policies): Allow users to register 3rd party policies - `pip install lerobot_policy_mypolicy` (#2308)
    * feat: Register external policies
    
    * ruff fix
    
    * move policy util functions to policy factory
    
    * refactor register_third_party_devices -> register_third_party_plugins
    
    * feat: Update docs with bring your own policies
    
    * Improve docs for new policies
    
    * fix: Inconsistent quotation marks
    
    * fix: Remove print statement
    
    * fix: wrong base class name in documentation
    
    * fix: Handle better how the models are parsed
    
    * fix: precommit passing
    
    * Update docs/source/bring_your_own_policies.mdx
    
    Co-authored-by: Steven Palma <imstevenpmwork@ieee.org>
    Signed-off-by: Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
    
    ---------
    
    Signed-off-by: Steven Palma <imstevenpmwork@ieee.org>
    Signed-off-by: Daniel San José Pro <42489409+danielsanjosepro@users.noreply.github.com>
    Co-authored-by: Steven Palma <imstevenpmwork@ieee.org>

--- src/lerobot/scripts/lerobot_replay.py
+++ src/lerobot/scripts/lerobot_replay.py
@@ -62,7 +62,7 @@ from lerobot.robots import (  # noqa: F401
     dusty
 )
 from lerobot.utils.constants import ACTION
-from lerobot.utils.import_utils import register_third_party_devices
+from lerobot.utils.import_utils import register_third_party_plugins
 from lerobot.utils.robot_utils import precise_sleep
 from lerobot.utils.utils import (
     init_logging,
@@ -128,7 +128,7 @@ def replay(cfg: ReplayConfig):
 
 
 def main():
-    register_third_party_devices()
+    register_third_party_plugins()
     replay()
 
 
